d4f61455df336bbd5fe293069d6b54cf8f374aaa
lgactna <48928674+lgactna@users.noreply.github.com>
2020-08-17 14:36:24 -0700

    get window screenshot

---
+++ tile-recognition/main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
+
+#see https://stackoverflow.com/questions/35097837/capture-video-data-from-screen-in-python
+
+mon = {'top': 0, 'left': 0, 'width': 400, 'height': 400}
+
+sct = mss()
+
+while 1:
+    screenshot = mss().grab(mon)
+    #https://python-mss.readthedocs.io/api.html#mss.tools.mss.base.ScreenShot
+    img = Image.frombytes("RGB", (screenshot.width, screenshot.height), screenshot.rgb)
+    #sct.get_pixels(mon)
+    #img = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
+    cv2.imshow('test', np.array(img))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
